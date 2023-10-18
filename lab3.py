49522d59acb7d04280f31d70ba1eeb07f2e0446c
Al-Bear-san <k.kadzuta@mail.ru>
2023-10-19 04:44:41 +0700

    Заказ напитка

--- lab3.py
+++ lab3.py
@@ -6,6 +6,7 @@ lab3 = Blueprint('lab3', __name__)
 def lab():
     return render_template('lab3.html')
 
+
 @lab3.route('/lab3/form1')
 def form1():
     errors = {}
@@ -18,5 +19,33 @@ def form1():
         errors['age'] = 'Заполните поле!'
 
     sex = request.args.get('sex')
-    
+
     return render_template('form1.html', user=user, age=age, sex=sex, errors=errors)
+
+
+@lab3.route('/lab3/order')
+def order():
+    return render_template('order.html')
+
+
+@lab3.route('/lab3/pay')
+def pay():
+    price = 0
+    drink = request.args.get('drink')
+    if drink == 'cofee':
+        price = 150
+    elif drink == 'black-tea':
+        price = 80
+    else:
+        price = 70
+    
+    if request.args.get('milk') == 'on':
+        price += 30
+    if request.args.get('sugar') == 'on':
+        price += 10
+    return render_template('pay.html', price=price)
+
+
+@lab3.route('/lab3/success')
+def success():
+    return render_template('success.html')
