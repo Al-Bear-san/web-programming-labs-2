131a287126d0323f0627d5457afe5df25607ca2c
Al-Bear-san <k.kadzuta@mail.ru>
2023-10-19 06:11:07 +0700

    Железнодорожный билет

--- lab3.py
+++ lab3.py
@@ -49,3 +49,58 @@ def pay():
 @lab3.route('/lab3/success')
 def success():
     return render_template('success.html')
+
+
+@lab3.route('/lab3/railway')
+def railway():
+    return render_template('railway.html')
+
+
+@lab3.route('/lab3/ticket')
+def ticket():
+    errors = {}
+    name = request.args.get('name')
+    if name == '':
+        errors['name'] = 'Заполните поле!'
+    
+    adulthood = request.args.get('adulthood')
+    if adulthood == 'adult':
+        adulthood = 'Взрослый'
+    else:
+        adulthood = 'Детский'
+    
+    shelf = request.args.get('shelf')
+    if shelf == 'lower':
+        shelf = 'Нижная'
+    elif shelf == 'upper':
+        shelf = 'Верхная'
+    elif shelf == 'upper_side':
+        shelf = 'Верхняя боковая'
+    else:
+        shelf = 'Нижняя боковая'
+
+
+    luggage = request.args.get('luggage')
+    if luggage == 'on':
+        luggage = 'Есть'
+    else:
+        luggage = 'Нет'
+    
+    age = request.args.get('age')
+    if age == '':
+        errors['age'] = 'Заполните поле!'
+
+    exit = request.args.get('exit')
+    if exit == '':
+        errors['exit'] = 'Заполните поле!'
+    
+    purpose = request.args.get('purpose')
+    if purpose == '':
+        errors['purpose'] = 'Заполните поле!'
+    
+    datapo = request.args.get('datapo')
+    if datapo == '':
+        errors['datapo'] = 'Заполните поле!'
+    
+    return render_template('ticket.html', name=name, luggage=luggage, age=age, adulthood=adulthood,
+                           shelf=shelf, exit=exit, purpose=purpose, datapo=datapo, errors=errors)
