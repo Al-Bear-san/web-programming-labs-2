f26442f2179b53143b0170e83cd40b7bebe7bbe1
Al-Bear-san <k.kadzuta@mail.ru>
2024-01-09 01:37:12 +0700

    Лабораторная работа 9

--- lab7.py
+++ lab7.py
@@ -3,11 +3,11 @@ lab7 = Blueprint("lab7",__name__)
 
 @lab7.route("/lab7/")
 def main():
-    return render_template('index.html')
+    return render_template('lab7/index.html')
 
 @lab7.route("/lab7/drink")
 def drink():
-    return render_template('drink.html')
+    return render_template('lab7/drink.html')
 
 @lab7.route("/lab7/api", methods = ['POST'])
 def api():
