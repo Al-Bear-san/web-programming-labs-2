2302d8b411fb12e6e3108af54d7764cf7de75829
Al-Bear-san <k.kadzuta@mail.ru>
2024-01-08 21:52:37 +0700

    Лабораторная работа 7

--- app.py
+++ app.py
@@ -1,11 +1,47 @@
-from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from Db import db
+from Db.models import users
+from flask_login import LoginManager
+from flask import Flask, session
 from lab1 import lab1
 from lab2 import lab2
 from lab3 import lab3
 from lab4 import lab4
+from lab5 import lab5
+from lab6 import lab6
+from lab7 import lab7
+#from lab8 import lab8
+#from lab9 import lab9
+
 
 app = Flask(__name__)
+app.secret_key = "123"
+user_db = "sypchenko_admin_knowledge_base"
+host_ip = "127.0.0.1"
+host_port = "5432"
+database_name = "knowledge_base_for_sypchenko"
+password = "12345678"
+
+app.config ['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{user_db}:{password}@{host_ip}:{host_port}/{database_name}'
+app.config ['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
+login_manager = LoginManager()
+
+login_manager.login_view = "lab6.login"
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_users(user_id):
+    return users.query.get(int(user_id))
+
+
 app.register_blueprint(lab1)
 app.register_blueprint(lab2)
 app.register_blueprint(lab3)
 app.register_blueprint(lab4)
+app.register_blueprint(lab5)
+app.register_blueprint(lab6)
+app.register_blueprint(lab7)
+#app.register_blueprint(lab8)
+#app.register_blueprint(lab9)
